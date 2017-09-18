639e5350f8d38a5ae767d656e5a0e1e717296104
NICOLAS TOUSSAINT <nicolas-externe.toussaint@erdf.fr>
2017-09-18 09:50:06 +0200

    updtae bash sourcing

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ _log() {
 	echo "$*"
 }
 
+_log
 _log "Setting up configuration files"
 cd $(dirname $0)
 cur_dir=$(pwd)
@@ -25,10 +26,18 @@ do
 	ln -sv $cur_dir/$file $link_file
 done
 
-_log "Appending sources to bashrc file"
-cat >> $HOME/.bashrc << EOS 
-[ -f ~/.bash_aliases ] && source ~/.bash_aliases
-[ -f ~/.bash_share ] && source ~/.bash_share
-EOS
+_log
+bash_share="$HOME/.bash_share"
+bash_rc="$HOME/.bashrc"
+if grep -q "^[^#].*source $bash_share" $bash_rc
+then
+	_log "DO NOT duplicate '$bash_share' sourcing in '$bash_rc'"
+else
+	_log "Appending sources to bashrc file"
+	cat >> $bash_rc <<-EOS
+	[ -f $bash_share ] && source $bash_share
+	EOS
+fi
 
+_log
 
