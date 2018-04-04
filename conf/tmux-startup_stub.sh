aa76a31eda998f08c1347340a7df22cda45db27a
Toussaint Nicolas <nicoolas.t@gmail.com>
2018-04-04 09:41:16 +0200

    update tmux-startup_stub.sh

--- conf/tmux-startup_stub.sh
+++ conf/tmux-startup_stub.sh
@@ -1,29 +1,52 @@
 #!/bin/sh
 
-f_new_window() {
-    WINDOW_ID=$((WINDOW_ID+1))
+# send-keys:
+# Ctrl keys may be prefixed with ‘C-’ or ‘^’, and Alt (meta) with ‘M-’.
+# The following special key names are accepted:
+#     Up, Down, Left, Right, BSpace, BTab, DC (Delete),
+#     End, Enter, Escape, F1 to F20, Home, IC (Insert),
+#     NPage/PageDown/PgDn, PPage/PageUp/PgUp, Space, and Tab.
+
+f_window() {
     WINDOW_NAME="$1"
-    echo -n ">>> $WINDOW_NAME"
+	shift 1
+	if [ "$WINDOW_ID" = "_" ]
+	then
+	    WINDOW_ID=0
+		tmux new-session -s $SESSION -d -n "$WINDOW_NAME" "$@"
+	else
+		tmux new-window -t $SESSION:$WINDOW_ID -n "$WINDOW_NAME" "$@"
+	fi
+    echo "--> $SESSION:$WINDOW_ID | $WINDOW_NAME"
+    WINDOW_ID=$((WINDOW_ID+1))
 }
 
-SESSION=_Session-1_
-WINDOW_ID=0
-tmux new-session -s $SESSION -d
+f_session() {
+	SESSION="$1"
+	WINDOW_ID="_"
+	echo
+    echo ">>> $SESSION"
+}
 
-f_new_window "htop"
-tmux new-window -t $SESSION:$WINDOW_ID -n "$WINDOW_NAME" -c ~/
-tmux send-keys "htop" C-m
+f_send_keys() {
+	sleep 0.2
+	for k in "$@"
+	do
+		tmux send-keys "$k"
+	done
+}
+
 
 #tmux split-window -t $SESSION:$WINDOW_ID -v -c ~/
 #tmux split-window -t $SESSION:$WINDOW_ID -h -c ~/
-tmux select-window -t $SESSION:0
+#tmux select-window -t $SESSION:0
 
-SESSION=_Session-2_
-WINDOW_ID=0
-tmux new-session -s $SESSION -d
 
-f_new_window "..."
-tmux new-window -t $SESSION:$WINDOW_ID -n "$WINDOW_NAME"
-tmux send-keys "# ..." C-m
+# = = = = = = = = = = = = = = = = = = =
+#DEFAULT_SESSION=0
+#f_session $DEFAULT_SESSION
+#f_window "_-_" -c ~/
 
+
+#tmux select-window -t $DEFAULT_SESSION
 
